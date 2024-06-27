bacfd3f77a8617041c792d73467f6165e6dfdcfc
Richard <richard232manansala@gmail.com>
2024-06-27 20:23:09 +0800

    Force dynamic in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/5ed187d4-d8f8-4f4d-a70b-d999ad79b364-ye995c.png",
   "https://utfs.io/f/79c8b911-6f7b-47d9-acea-46281d2531a8-clvff2.png",
