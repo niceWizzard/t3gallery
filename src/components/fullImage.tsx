c43568fc4041f83aba716fd0f7cbc54a7aa491cb
Richard <richard232manansala@gmail.com>
2024-06-28 13:20:12 +0800

    Added nav in fullIamge

--- src/components/fullImage.tsx
+++ src/components/fullImage.tsx
@@ -3,6 +3,13 @@ import { getImage } from "~/server/db/queries/images";
 export default async function FullPageImagePreview({ id }: { id: string }) {
   const image = await getImage({ id: parseInt(id) });
   return (
-    <img src={image.url} alt={image.name} className="w-36 object-contain" />
+    <div className="flex h-full w-full ">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} alt={image.name} className=" object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <span className="text-xl font-bold">{image.name}</span>
+      </div>
+    </div>
   );
 }
